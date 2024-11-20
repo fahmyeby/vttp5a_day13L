9bde615a5af58d7a4c669e7f14e024089c5e5bc8
fahmyeby <fahmyebyyussoff@yahoo.com.sg>
2024-11-20 09:04:17 +0800

    first push

---
+++ src/main/java/nus/edu/iss/vttp_day13l/VttpDay13lApplication.java
@@ -0,0 +1,13 @@
+package nus.edu.iss.vttp_day13l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VttpDay13lApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VttpDay13lApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nus/edu/iss/vttp_day13l/VttpDay13lApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.edu.iss.vttp_day13l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VttpDay13lApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
